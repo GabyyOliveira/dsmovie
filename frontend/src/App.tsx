ed77ec7f29676f360cb9c0d2a339ae1e8bc3e84e
Gabs <gabyy.oliveira33@gmail.com>
2022-05-10 20:46:54 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,26 @@
-import Navbar from "./components/Navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from './pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-   <Navbar />
+    //iniciar a configuração das rotas
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        {/* na rota localhost:3000 abre a lista de filmes */}
+        <Route path="/" element={<Listing />} />
+        {/* na rota localhost:3000/form/idfilme abre a pagina de formulario*/}
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
